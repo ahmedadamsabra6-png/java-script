0fb707b0c6c4f7aab5964797b84449f63531f216
ahmedadamsabra6-png <ahmedadamsabra6@gmail.com>
2026-05-15 20:34:40 +0300

    Add files via upload

---
+++ hometask3.js
@@ -0,0 +1,26 @@
+x = prompt('Enter a Number From 1 to 12 and a month: ')
+if (x == '1 January') {
+    alert('31')
+}if (x == '2 February') {
+    alert('28 or 29')
+}if (x == '3 March') {
+    alert('31')
+}if (x == '4 April') {
+    alert('30')
+}if (x == '5 May') {
+    alert('31')
+}if (x == '6 June') {
+    alert('30')
+}if (x == '7 July') {
+    alert('31')
+}if (x == '8 August') {
+    alert('31')
+}if (x == '9 September') {
+    alert('30')
+}if (x == '10 October') {
+    alert('31')
+}if (x == '11 November') {
+    alert('31')
+}if (x == '12 December') {
+    alert('31')
+}
